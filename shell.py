ac6b270cce3bb9763f9b62f3165e7acfee2ac32b
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-02-10 17:44:13 +0530

    v0.03 touch rm added and COLOURS

--- shell.py
+++ shell.py
@@ -3,16 +3,33 @@ import os
 import re
 import getpass
 import sys
+import readline
+readline.parse_and_bind("tab: complete")
 
+# up = '\x1b[A'
+# down = '\x1b[B'
+# left = '\x1b[D'
+# right = '\x1b[C'
+
+reset = '\033[0m'
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+purple = '\033[35m'
+cyan = '\033[36m'
 
 def cd(path):
     os.chdir(path)
 
+
 def pwd():
     print(os.getcwd())
 
+
 def whoami():
     print(getpass.getuser())
+
 
 def ls():
     ls = os.listdir(os.curdir)
@@ -23,18 +40,28 @@ def ls():
     for obj in ls:
         print(obj.ljust(max_file_len),end="")
         counter+=1
-        if counter%3==0:
+        if counter%3==0 or counter == len(ls):
             print()
+
 
 def clear():
     # Got this using clear | hexdump -C
     print("\x1b\x5b\x48\x1b\x5b\x32\x4a", end="")    
-    
 
-up = '\x1b[A'
-down = '\x1b[B'
-left = '\x1b[D'
-right = '\x1b[C'
+
+def touch(filename):
+    if not os.path.exists(filename):
+        open(filename, 'a').close()
+    else:
+        print("file already exists")
+
+
+def rm(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+    else:
+        print("Invalid path")
+
 
 # This will make available = ['cd', 'path', ... with all functions above next line]
 available = []
@@ -43,29 +70,32 @@ for key, value in local_locals:
     if callable(value) and value.__module__ == __name__:
         available.append(key)
 
-class Exit(Exception):
-    pass
-
 def runShell():
     cwd = os.getcwd()
     dirname = os.path.split(cwd)[-1]
     user = getpass.getuser()
-    PS1 = f"{user} {dirname} $ "
+    PS1 = f"{cyan}{user}{reset} {green}{dirname}{reset} {purple}${reset} "
 
     # func_cleanup = re.compile(f"({'|'.join(available)})")
 
     command = input(PS1)
 
-    possible_types = {1: 'Python', 2: 'Shell', 3: 'Invalid'}
+    # possible_types = {1: 'Python', 2: 'Shell', 3: 'Invalid'}
 
     old_stdout = sys.stdout
     try:
         sys.stdout = None 
-        splut = command.split()
-        if command.split()[0] in available:
+        splitted = command.split(" ")
+        print("splitted:", splitted)
+        if splitted[0] in available:
             command_type = 2
-            if len(splut) == 1:
+            if len(splitted) == 1:
                 command += '()'
+            else:
+                splitted.insert(1,"('")
+                splitted.append("')")
+                command = ''.join(splitted)
+                print(command)
         elif eval(command) is None:
             command_type = 1
     except:
@@ -73,7 +103,7 @@ def runShell():
     sys.stdout = old_stdout
 
     if command == 'exit' or command == 'quit':
-        raise Exit
+        raise KeyboardInterrupt
     elif command_type == 3:
         print(f"Invalid command {user}!")
     else:
@@ -83,6 +113,8 @@ try:
     while True:
         runShell()
 
-except (KeyboardInterrupt, EOFError, Exit):
-    print("\b\bExiting cleanly")
+except KeyboardInterrupt:
+    print()
+    print(yellow+"Exiting cleanly"+reset)
+    print()
     exit(1)
---
+++ testcolors.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+for i in range (29,50):
+    print(f"\x1b[{i}m {i}\x1b[0m")
+
