9e43b8ec5a022a17e97687a32e49756a5bb2a47b
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-02-11 12:03:15 +0530

    v0.81 better chat

--- irc_client.py
+++ irc_client.py
@@ -3,33 +3,27 @@ import readline
 import threading
 import socket
 import sys
-import os
-import signal
-import time
 
-nickname = input("Enter a username: ")
+ping = 'PING '
+pong = 'PONG '
+nickname = input('Enter a username: ')
 nick_tosend = f'NICK {nickname} \r\n'
 name_tosend = 'USER ' + nickname + ' 0 * :' + 'blah' + '\r\n'
 host = ('kaushik.me',4444)
 
 # Connect.
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host[0], int(host[1])))
+client.connect((host[0], host[1]))
 
 # Handshake.
 client.send((nick_tosend).encode())
 client.send((name_tosend).encode())
 client.send(b'JOIN #general \r\n')
 
-stop = False
-
 data = ""
 prevData = "not same as data"
 def recvData():
-    global data, stop
-    if msg == 'exit':
-        stop = True
-        return
+    global data
     data = client.recv(512).decode()
     for line in data.split('\n'):
         if 'PRIVMSG' in line:
@@ -38,55 +32,43 @@ def recvData():
 
 msg = ""
 prevMsg = "not same as msg"
-
 def takeInput():
-    global msg, stop
+    global msg
     msg = input("msg : ")
-    if msg == 'exit':
-        stop = True
-        return
-
     client.send(('PRIVMSG #general :'+msg+' \r\n').encode())
-
 
 # Output and ping/pong.
 firstRun = True
-try:
-    while True:
-        if msg != prevMsg:
-            prevMsg = msg
+while True:
+    if data != prevData:
+        prevData = data
+        if not firstRun:
+            if recvThread.isAlive():
+                recvThread.join()
+        recvThread = threading.Thread(target=recvData)
+        recvThread.daemon = True
+        recvThread.start()
 
-            if not firstRun:
-                if inputThread.isAlive():
-                    inputThread.join()
-            inputThread = threading.Thread(target=takeInput)
-            inputThread.daemon = True
-            inputThread.start()
+    # else:
+    #     print(data)
 
-        if data != prevData:
-            prevData = data
+    
+    if msg != prevMsg:
+        prevMsg = msg
+        if not firstRun:
+            if inputThread.isAlive():
+                inputThread.join()
+        inputThread = threading.Thread(target=takeInput)
+        inputThread.daemon = True
+        inputThread.start()
 
-            if not firstRun:
-                if recvThread.isAlive():
-                    recvThread.join()
-            recvThread = threading.Thread(target=recvData)
-            recvThread.daemon = True
-            recvThread.start()
+    firstRun = False
 
-        time.sleep(100)
+    if data.startswith(ping):
+        resp = data.strip('PING ');
+        client.send(('PONG ' + resp).encode())
 
-        firstRun = False
-
-        # else:
-        #     print(data)
-
-        if stop:
-            raise KeyboardInterrupt
-
-        if data.startswith(ping):
-            resp = data.strip('PING ');
-            client.send(('PONG ' + resp).encode())
-except KeyboardInterrupt:
-    stop = True
-    while recvThread.isAlive():
-        recvThread.join(1)
+    if msg == 'exit':
+        recvThread.join()
+        inputThread.join()
+        break
