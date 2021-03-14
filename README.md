8fd683b0e52f9085dee2b809839aa6fba613beb4
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-03-14 12:33:06 +0530

    v1.1 kedit works with multipage files; kedit and shell commented

--- README.md
+++ README.md
@@ -1,22 +1,44 @@
-# Pysh
-A python REPL with realtime IRC chat!
+# Shelly
+A no external dependancy bash-like python REPL with a realtime IRC chat and custom text editor.
 
 # Usage 
 clone this repo and run `python3 shell.py`.
 
-# Screenshots
+# Screenshots  
 
-![Screenshot 2021-02-10 at 6 44 08 PM](https://user-images.githubusercontent.com/59250093/107514746-1125ed80-6bd0-11eb-917e-a8565de413e8.png)
-![Screenshot 2021-02-11 at 12 06 26 PM](https://user-images.githubusercontent.com/59250093/107726269-68ca7300-6d0e-11eb-8c1d-d20beafb132d.png)
+## Shell
+![Screenshot 2021-03-14 at 12 19 55 PM](https://user-images.githubusercontent.com/59250093/111059975-99452e80-84bf-11eb-9167-5259123c8a58.png)
+
+## Chat
+![Screenshot 2021-03-14 at 12 25 16 PM](https://user-images.githubusercontent.com/59250093/111060071-59327b80-84c0-11eb-8ee7-e70119377da6.png)
+
+## Editor
+![Screenshot 2021-03-14 at 12 28 07 PM](https://user-images.githubusercontent.com/59250093/111060136-bd553f80-84c0-11eb-902f-5f4986d4f018.png)
+![Screenshot 2021-03-14 at 12 28 27 PM](https://user-images.githubusercontent.com/59250093/111060146-c9410180-84c0-11eb-9fe3-8b10aeb4da98.png)
+
+
+
+# Features
+- All basic shell commands.
+- Python code interpretation.
+- Coloured prompt and error messages.
+- Tab completion.
+- Custom text editor - kedit.
+- IRC chat.
+- no external dependancies (handwritten python all the way).
 
 # TODO
-- Remove all external dependancies
-- No dependancy text editor
-- Pipe support
-- Redirect operator implementation
-- sudo support
-- chmod support  
-- Autojump support
+- Pipe support.
+- Redirect operator implementation. 
+- sudo support. 
+- chmod support. 
+- Autojump support. 
+- remote command execution (with consent) via IRC.  
+- add support for running external binaries. 
 
 # DEVELOPMENT
 To add your own commands simply define them as a function above the `DEFINE YOUR FUNCTION ABOVE THIS LINE` line.
+
+# CREDITS
+- scripts/cow.py customized from https://github.com/VaasuDevanS/cowsay-python.
+- readchar from https://github.com/magmax/python-readchar.
--- scripts/kedit.py
+++ scripts/kedit.py
@@ -6,16 +6,19 @@ import string
 from time import sleep
 from copy import deepcopy
 
+from readchar import readchar
+
 script_loc = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(script_loc))
 
-from readchar import readchar
-
 iswin = sys.platform.startswith('win')
 if iswin:
+    # Must run this in windows for colour codes to work
     os.system('color')
 
 def editFile(path=None):
+    
+    # Make Untitled.txt if there isn't a path
     if path is None:
         path = 'Untitled.txt'
 
@@ -27,128 +30,174 @@ def editFile(path=None):
     purple = '\033[35m'
     cyan = '\033[36m'
 
-    columns = shutil.get_terminal_size().columns
-    center_row = (shutil.get_terminal_size().lines-1)//2
+    # Key codes - up is \1xb[A and only the last character is unique so just store that
+    # Index 1 is cursor's direction count
     up = [b'A',0]
     down = [b'B',0]
     left = [b'D',0]
     right = [b'C',0]
+
+    # Windows uses a different hex form backspace (use xxd -psg to confirm)
     backspace = b'\x7f' if not iswin else b'\x08'
 
+    # If our file exists open it up and read it into towrite
     towrite = ''
-    prev_towrite = ''
     if os.path.isfile(path):
         with open(path,'r+') as f:
             towrite += f.read().rstrip()
+    lines = towrite.split('\n')
+    scroll = 0
 
-    # curs_row = curs_col = lambda : 1
-    # rows=0
+    # Rows and Columns in our terminal window
+    columns = shutil.get_terminal_size().columns
+    termrows = shutil.get_terminal_size().lines-1
 
-    write = False
     clear_hex = "\x1b\x5b\x48\x1b\x5b\x32\x4a"
+
+    # Print without newline
     fprint = lambda x: print(x,end="",flush=True)
+
+    # Clear screen
     clr = lambda: fprint(clear_hex)
+
+    # Print banner
     banner = lambda: print(clear_hex+"\x1b[30;37m"+'KEDIT V1'.center(columns)+"\x1b[0m")
 
+    # Start banner
     welcome = [f'Editing {path}',
                f'Hit escape twice to save and exit',
                f'Any other key after escape to exit without saving',
                f'Press any key to continue']
     colors = [yellow,green,red,purple]
-    clr()
     banner()
+    # This will hide the cursor
     fprint('\033[?25l')
-    fprint('\n'*(center_row-2))
+    fprint('\n'*(termrows//2-2))
     for i in range(4):
         print(colors[i]+welcome[i].center(columns)+reset)
     readchar.readchar()
+    # This will show cursor
     fprint('\033[?25h')
 
     while True:
+        # print title banner
         banner()
-        fprint(towrite)
 
-        upcount = up[1]-down[1]
-        fprint(f'\x1b[{up[0].decode("utf-8")}'*upcount)
+        # Might need to scroll screen if file is long
+        # we will calculate that later
+        fprint('\n'.join(lines[scroll:termrows+scroll]))
 
-        leftcount = left[1]-right[1]
-        if leftcount>=0:
-            fprint(f'\x1b[{left[0].decode("utf-8")}'*leftcount)
-        else:
-            fprint(f'\x1b[{right[0].decode("utf-8")}'*-leftcount)
+        # Take cursor to top left of screen
+        fprint('\x1b[2;1H')
 
+        # Go to cursor's position by calculating effective y
+        # and x from the top left and move by that much
+        downcount = down[1]-up[1]
+        rightcount = right[1]-left[1]
+        fprint(f'\x1b[{down[0].decode("utf-8")}'*downcount)
+        fprint(f'\x1b[{right[0].decode("utf-8")}'*rightcount)
+
+        # Read a character
         char = readchar.readchar()
+
+        # Windows readchar() returns byte and darwin returns string
+        # So must have functions to interconvert
         byte_char = lambda: char if type(char)==bytes else char.encode()
         actual_char = lambda: char.decode('utf-8') if type(char)==bytes else char
-        key_is = lambda x: byte_char()==x
-        temp = towrite.split('\n')
-        rows = len(temp)-1
-        curs_row = lambda : rows-up[1]+down[1]
-        curs_col = lambda : len(temp[-1])-1-left[1]+right[1]
 
+        # Check if char is a specific byte
+        key_is = lambda x: byte_char()==x
+
+        # All editing is done within a split array
+        # joined in last line
+        lines = towrite.split('\n')
+        linecount = len(lines)-1
+        curs_row = lambda : down[1]-up[1]
+        curs_col = lambda : right[1]-left[1]
+
+        # If key is escape
         if key_is(b'\x1b'):
             char = readchar.readchar()
+            # if next character is '[' it is probably an arrow key
             if key_is(b'['):
                 char = readchar.readchar()
                 if key_is(up[0]):
                     if curs_row()-1>=0:
                         up[1]+=1
-                        if curs_col()>len(temp[curs_row()])-1:
-                            left[1] = len(temp[-1])-len(temp[curs_row()])
-                            right[1] = 0;
+                        if curs_col()>len(lines[curs_row()])-1:
+                            right[1] = len(lines[curs_row()])
+                            left[1] = 0;
                 if key_is(down[0]):
-                    if curs_row()+1<=rows:
+                    if curs_row()+1<=linecount:
                         down[1]+=1
-                        if curs_col()>len(temp[curs_row()])-1:
-                            left[1] = len(temp[-1])
-                            right[1] = 0;
+                        if curs_col()>len(lines[curs_row()])-1:
+                            right[1] = len(lines[curs_row()])
+                            left[1] = 0
                 if key_is(right[0]):
-                    if curs_col()+1<len(temp[curs_row()]):
+                    if curs_col()<len(lines[curs_row()]):
                         right[1]+=1
                 if key_is(left[0]):
-                    if curs_col()-1>=-1:
+                    if curs_col()-1>=0:
                         left[1]+=1
+
+            # Pressing escape twice means save and exit
             elif key_is(b'\x1b'):
                 write = True
                 break
+
+            # Pressing something after escape means exit without save
             else:
                 write = False
                 break
 
+        # Otherwise this is probably a normal character
         else:
+            # In windows pressing enter returns linefeed not newline -_-
             if actual_char()=='\r':
                 char = '\n'
 
-            toedit = temp[curs_row()]
+            # Lets edit the row we are in then reassign to line[curs_row()]
+            toedit = lines[curs_row()]
             if actual_char() in string.printable:
-                toedit = toedit[:curs_col()+1]+actual_char()+toedit[curs_col()+1:]
-                temp[curs_row()] = toedit
+                if char=='\t':
+                    char = ' '*4
+                toedit = toedit[:curs_col()]+actual_char()+toedit[curs_col():]
+                lines[curs_row()] = toedit
                 if char=='\n':
-                    if curs_row() == rows:
-                        left[1] = len(toedit[curs_col():])-1
-                    else:
-                        left[1] = len(temp[-1])
+                    left[1] = 0
                     right[1] = 0
-                elif curs_row()!=len(temp)-1:
-                        right[1]+=1
-
-            elif key_is(backspace):
-                if curs_col()==-1:
-                    if not curs_row()==0:
-                        temp[curs_row()-1]+=toedit
-                        temp.pop(curs_row())
+                    down[1] += 1
                 else:
-                    toedit = toedit[:curs_col()]+toedit[curs_col()+1:]
-                    temp[curs_row()] = toedit
-                    if curs_row()!=rows:
-                        left[1]+=1
+                    right[1]+=len(char)
 
-            towrite = '\n'.join(temp)
+            # If key is backspace remove the character before the cursor
+            elif key_is(backspace):
+                if curs_col()==0:
+                    if not curs_row()==0:
+                        right[1] = len(lines[curs_row()-1])
+                        left[1] = 0
+                        lines[curs_row()-1]+=toedit
+                        lines.pop(curs_row())
+                        up[1] += 1
+                else:
+                    toedit = toedit[:curs_col()-1]+toedit[curs_col():]
+                    lines[curs_row()] = toedit
+                    left[1]+=1
+
+            towrite = '\n'.join(lines)
+
+        # If our total lines is greater than terminal length 
+        # we could need to scroll down if cursor is on the last line
+        if linecount>termrows:
+            check_scroll = (down[1]+1)-(up[1])-termrows
+            if check_scroll>0:
+                scroll = check_scroll
 
     towrite = towrite.rstrip('\n')+'\n'
 
 
     clr()
+    # Save and exit if write flag set if not exit
     if write:
         print(f"{green}Saving {path}{reset}")
         with open(path,'w+') as f:
